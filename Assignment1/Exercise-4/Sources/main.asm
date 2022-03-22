617e641f0ea258837899bba61570fc0f0eeb0c9a
efos4845 <efos4845@uni.sydney.edu.au>
2022-03-22 16:48:21 +1100

    Cleaned up ex4 code

--- Assignment1/Exercise-4/Sources/main.asm
+++ Assignment1/Exercise-4/Sources/main.asm
@@ -43,13 +43,15 @@ _Startup:
             
             CLI                   ;enable interrupts
             
-            BRA interupt_demo
+            BRA interupt_demo     ;change to whatever you want to demo
             
-            
+;**************************************************************
+;*                         Interupts                          *
+;**************************************************************            
 
 interupt_demo:
-            MOVB #mTSCR1_TEN, TSCR1
-            MOVB #mTSCR2_TOI, TSCR2
+            MOVB #mTSCR1_TEN, TSCR1  ;enable the timer
+            MOVB #mTSCR2_TOI, TSCR2  ;set the prescaler value to 1
             BSET TFLG2, #mTFLG2_TOF
             MOVB #$FF, PORTB
 empty_loop:
@@ -59,7 +61,9 @@ empty_loop:
             
             
             
-
+;**************************************************************
+;*                    Subroutine Timer                        *
+;**************************************************************
 
             
 subroutine_timer:      
@@ -78,9 +82,7 @@ subroutine_timer:
                        
             
 
-
-
-dummy_subroutine:
+dummy_subroutine:                ;random operations to test subroutine timer
             LDAA #$10
             SUBA #$01
             LDAB #41
@@ -94,11 +96,11 @@ dummy_subroutine:
 ;**************************************************************
 
 variable_delay_demo:       
-            MOVB #$FF, PORTB
-            JSR variable_delay
-            CLR PORTB
-            JSR variable_delay
-            BRA variable_delay_demo
+            MOVB #$FF, PORTB        ;turn on the LEDs
+            JSR variable_delay      ;delay
+            CLR PORTB               ;turn off LEDs
+            JSR variable_delay      ;delay
+            BRA variable_delay_demo ;loop
 
 variable_delay:
             MOVB #$90, TSCR1       ;enable the time and fast flag clear
@@ -121,19 +123,19 @@ delay_loop_inner:
 ;**************************************************************
             
 timer_overflow:
-            INC counter
-            BVS counter_overflow
+            INC counter            ;if timer overflows increment counter
+            BVS counter_overflow   ;if counter overflows branch
 end_isr:
-            LDX TCNT
-            BSET TFLG2, #mTFLG2_TOF
-            RTI
+            LDX TCNT                 ;get timer for debugging in simulator
+            BSET TFLG2, #mTFLG2_TOF  ;reset interrupt flag
+            RTI                      ;exit isr
 
 
 
 
-counter_overflow:         
-            MOVB #$80, PORTB
-            JSR fixed_delay
+counter_overflow:                    ;flash on each LED from MSB to LSB with short delay
+            MOVB #$80, PORTB         ;this gives a visual to demonstrate a function being
+            JSR fixed_delay          ;called periodically to a high accuracy
             MOVB #$40, PORTB
             JSR fixed_delay
             MOVB #$20, PORTB
@@ -149,10 +151,10 @@ counter_overflow:
             MOVB #$01, PORTB
             JSR fixed_delay
             
-            BRA end_isr
+            BRA end_isr              ;branch top finish isr
             
             
-fixed_delay:
+fixed_delay:                         ;short delay subroutine to make LED visuals easier to see
             LDY #$FFFF
             fixed_delay_loop:
               DEY
@@ -170,4 +172,4 @@ fixed_delay:
             DC.W  Entry           ; Reset Vector
             
             ORG   $FFDE
-            DC.W timer_overflow
+            DC.W timer_overflow   ; Timer overflow interupt
